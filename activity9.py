f3d300517abafb094e1913fa4dccca14609d334e
Joan-Jasmin <namjabyeog@gmail.com>
2025-08-20 18:55:28 +0800

    Add files via upload
    August 20, 2025

---
+++ activity9.py
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+c = 1
+d = 1
+
+username = 'Jasper'
+password = 'GoldenFox'
+print((username == 'Jasper') and (password == 'GoldenFox'))
+
+print( (a > b) and (c < d) )
+print( (a > b) or (c < d) )
+print(not((a > b) or (c < d)))
