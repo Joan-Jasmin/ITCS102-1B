5ddebeffd608897da6734ea9c9dc0295722e68ba
Joan-Jasmin <namjabyeog@gmail.com>
2025-08-27 15:15:30 +0800

    Add files via upload
    August 27, 2025

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Jasper"
+password = "GoldenFox"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+	print("GOOD JOB!")
+else:
+	print("GET OUT!")
