7604fc4f0789b1f088ed629b022d9befe8036331
Joan-Jasmin <namjabyeog@gmail.com>
2025-09-17 20:51:28 +0800

    Add files via upload
    September 17, 2025

---
+++ code_challenge7.py
@@ -0,0 +1,4 @@
+start = int(input("Enter a starting number for the countdown: "))
+for i in range(start, 0, -1):
+    print(i)
+print("Liftoff!")
---
+++ code_challenge9.py
@@ -0,0 +1,4 @@
+phrase = input("What should the parrot say? ")
+times = int(input("How many times should the parrot repeat it? "))
+for i in range(times):
+    print("Squawk! " + phrase)
